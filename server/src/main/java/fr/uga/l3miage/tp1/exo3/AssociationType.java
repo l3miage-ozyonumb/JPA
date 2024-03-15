ea4ae6ab3bc4b41ac13589aa7ed7193f8b0556e4
l3miage-ozyonumb <baran.ozyonum@etu.univ-grenoble-alpes.fr>
2024-03-15 14:03:45 +0100

    last

---
+++ server/src/main/java/fr/uga/l3miage/tp1/exo3/AssociationType.java
@@ -0,0 +1,5 @@
+package fr.uga.l3miage.tp1.exo3;
+
+public enum AssociationType {
+    ETU,OLD,OTHER
+}
---
+++ server/src/main/java/fr/uga/l3miage/tp1/exo3/BrandType.java
@@ -0,0 +1,7 @@
+package fr.uga.l3miage.tp1.exo3;
+
+public enum BrandType {
+    ALIMENTAIRE,
+    HYGIENE,
+    AUTRE
+}
